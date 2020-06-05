f09f7bb9aedbe9412c22a59fdcd0ae4faa57d619
c0wb3ll <48816875+c0wb3ll@users.noreply.github.com>
2020-06-05 10:19:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function oksSwitchTab(tab_id, tab_content) {
-    // first of all we get all tab content blocks (I think the best way to get them
-    // by class names)
     var x = document.getElementsByClassName("tabcontent");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.display = 'none'; // hide all tab content
+        x[i].style.display = 'none';
     }
-    document.getElementById(tab_content).style.display = 'block'; // display the content of the tab we need
-    // now we get all tab menu items by class names (use the next code only if you need to highlight current tab)
+    document.getElementById(tab_content).style.display = 'block';
+    
     var x = document.getElementsByClassName("tabmenu");
     var i;
     for (i = 0; i < x.length; i++) {
